7403903f31f75a0ab0e17c0e2b09b7d25aaeaa14
Koflaaa <taylor.kofler@itlabs.at>
2025-09-22 13:19:59 +0200

    Update CelciustoFahrenheit.py

--- math/convert_units/CelciustoFahrenheit.py
+++ math/convert_units/CelciustoFahrenheit.py
@@ -1,1 +1,17 @@
-
+while True:
+    grad_auswahl = int(input("Wählen Sie aus was sie machen möchten:\n1: Celsius zu Fahrenheit\n2: Fahrenheit zu Celsius\nAuswahl:"))
+    if int(grad_auswahl)==1:
+        C = int(input("Geben Sie einen Temperaturwert in °C ein: "))
+        Fahrenheit = (C * (9/5) + 32)
+        print(f"{C}°C in Fahrenheit beträgt: {Fahrenheit}°F")
+    elif int(grad_auswahl)==2:
+        F = float(input("Geben Sie einen Temperaturwert in °F ein: "))
+        Celsius = 5/9 * (F-32)
+        print(f"{F}°F zu Celsius beträgt: {Celsius}°C")
+    else:
+        print("Eingabe ungültig bitte erneut eingeben.")
+    entscheidung = input("Möchten Sie einen weiteren Wert umwandeln (Y/N)?: ").lower()
+    if entscheidung == "y":
+        continue
+    else:
+        break
