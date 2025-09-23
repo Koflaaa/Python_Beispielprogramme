39117f6cfe9d7207ef4d5ec3548853d59cdefeb3
Koflaaa <taylor.kofler@itlabs.at>
2025-09-23 11:05:44 +0200

    Update KilometerToMile.py

--- math/convert_units/KilometerToMile.py
+++ math/convert_units/KilometerToMile.py
@@ -1,1 +1,17 @@
-
+while True:
+    auswahl = int(input("Wählen Sie aus was sie machen möchten:\n1: Kilometer zu Meilen\n2: Meilen zu Kilometer\nAuswahl: "))
+    if int(auswahl)==1:
+        km = float(input("Geben Sie eine Kilometer-Anzahl ein: "))
+        mi = km * 0.621371
+        print(f"{km} km sind {mi} mi")
+    elif int(auswahl)==2:
+        mi = float(input("Geben Sie eine Meilen-Anzahl ein: "))
+        km = mi * 1.60934
+        print(f"{mi} mi sind {km} km")
+    else:
+        print("Eingabe ungültig bitte erneut eingeben.")
+    entscheidung = input("Möchten Sie einen weiteren Wert umwandeln (Y/N)?: ").lower()
+    if entscheidung == "y":
+        continue
+    else:
+        break
