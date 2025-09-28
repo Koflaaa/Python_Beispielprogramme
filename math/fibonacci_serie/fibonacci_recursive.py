652164fc425bfcab396726605141a3211234c7dc
Koflaaa <taylor.kofler@itlabs.at>
2025-09-28 20:18:54 +0200

    Update fibonacci_recursive.py

--- math/fibonacci_serie/fibonacci_recursive.py
+++ math/fibonacci_serie/fibonacci_recursive.py
@@ -1,1 +1,17 @@
-
+def fibonacci_rec(n):
+    if n <= 0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fibonacci_rec(n-1) + fibonacci_rec(n-2)
+    
+def main():
+    n = int(input("Wie viele Fibonacci-Zahlen sollen berechnet werden: "))
+    reihe = []
+    for i in range(n):
+        reihe.append(fibonacci_rec(i))
+    print(f"Die ersten {n} Fibonacci-Zahlen:")
+    print(reihe)
+    
+main()
